ba268e98a04d92da03666bf167735a092a147661
Georgi Filipov <georgi.filipov@mentormate.com>
2023-08-19 12:14:40 +0300

    Add version tag to sql resource

--- main.tf
+++ main.tf
@@ -87,6 +87,7 @@ resource "azurerm_mssql_server" "sql_srv" {
   name                         = "${local.prefix}-srv"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
   administrator_login          = random_password.sqluser.result
   administrator_login_password = random_password.sqlpassword.result
 }
