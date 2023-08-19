2b95ab28a7f2f71729773aa1dc5c581fbb2b7a01
Georgi Filipov <georgi.filipov@mentormate.com>
2023-08-19 04:15:22 +0300

    Add resource group to tf config

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 terraform {
+  required_version = ">= 1.3"
   backend "azurerm" {
+    resource_group_name  = "demo-shared"
     storage_account_name = "mmdemoterraform"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>3.57.0"
+    }
+  }
+}
+# Configure the Azure provider
+provider "azurerm" { 
+  features {}  
 }
 
 # An example resource that does nothing.
