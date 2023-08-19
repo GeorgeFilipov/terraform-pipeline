92179d3e139e46c60f7f265b48981b81fca8fc35
Georgi Filipov <georgi.filipov@mentormate.com>
2023-08-19 05:07:30 +0300

    Change mmdemo-weather-app to mmdemo-weather-api

--- main.tf
+++ main.tf
@@ -33,7 +33,6 @@ locals {
   }
 }
 
-# An example resource that does nothing.
 resource "azurerm_resource_group" "rg" {
   location = "westeurope"
   name     = "${local.prefix}-rg"
@@ -49,9 +48,9 @@ resource "azurerm_service_plan" "asp" {
   tags = local.default_tags
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "weather_api" {
 
-  name                = "mmdemo-weather-app"
+  name                = "mmdemo-weather-api"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.asp.location
   service_plan_id     = azurerm_service_plan.asp.id
